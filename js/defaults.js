1c618466bed1c4167ef7203ed21c8e1f34bc326d
Paul Sawchuk <contact-paul@rustynomad.com>
2024-07-10 11:00:18 -0700

    initial commit

---
+++ js/defaults.js
@@ -0,0 +1,23 @@
+var unitsInChart = 8
+
+var encounterMetrics = {
+    "Encounter": "title",
+    "Duration": "duration",
+    "DMG": "damage-*",
+    "DPS": "encdps-*",
+    "Healed": "healed",
+    "DMG Taken": "damagetaken-*",
+}
+
+var combatantsMetrics = {
+    "Name": "name",
+    "DMG": "damage-*",
+    "DPS": "encdps",
+    "Crit%": "crithit%",
+}
+
+var colorSeed = 127
+
+var colorOverrides = {
+    "Tristol": "00aaff"
+}
---
+++ js/eq2Overlay.js
@@ -0,0 +1,144 @@
+function init() {
+    encounter.init()
+    combatants.init()
+    addOverlayListener("CombatData", (e) => update(e))
+    startOverlayEvents()
+}
+
+function update(data) {
+    encounter.clear()
+    if (typeof (data.Encounter) == "object") {
+        encounter.update(data.Encounter)
+    }
+    combatants.clear()
+    if (typeof (data.Combatant) == "object") {
+        combatants.update(data.Combatant)
+    }
+
+}
+
+function percentToInt(percent) {
+    return parseInt(percent.replace(/^\%+|\%+$/g, ''))
+}
+
+function combatantDataSet(data) {
+    var dataSet = []
+    var count = 0
+    var maxPercent = 100
+    for (let key in data) {
+        if (count++ <= unitsInChart & data.hasOwnProperty(key)) {
+            if (count == 1) {
+                maxPercent = percentToInt(data[key]["damage%"])
+                if (maxPercent < 80) { maxPercent = maxPercent + 10 }
+            }
+            var curPercent = percentToInt(data[key]["damage%"])
+            data[key].displayPercent = Math.trunc((curPercent / maxPercent) * 100)
+            dataSet.push(data[key])
+        }
+    }
+
+    return dataSet
+}
+
+function makeHeader(metrics, tr) {
+    for (let key in metrics) {
+        if (metrics.hasOwnProperty(key)) {
+            var th = document.createElement("th")
+            th.innerText = key
+            tr.appendChild(th)
+        }
+    }
+
+}
+
+function makeRow(metrics, data) {
+    var tr = document.createElement("tr")
+
+    for (let key in metrics) {
+        if (metrics.hasOwnProperty(key)) {
+            var td = document.createElement("td")
+            td.innerText = data[metrics[key]]
+            tr.appendChild(td)
+        }
+    }
+
+    return tr
+}
+
+function colorHex(str) {
+    if (colorOverrides[str]) {
+        return colorOverrides[str]
+    }
+
+    var hash = parseInt(colorSeed)
+
+    if (str.length === 0) return hash
+    for (var i = 0; i < str.length; i++) {
+        var chr = str.charCodeAt(i)
+        hash = ((hash << 5) - hash) + chr
+        hash |= 0
+    }
+
+    colorOverrides[str] = Math.abs(hash).toString(16).substring(0, 6)
+    return colorOverrides[str]
+}
+
+
+function barBackground(name, percent) {
+    var rgb = colorHex(name)
+
+    return `linear-gradient(90deg, #${rgb}99 0%, #${rgb}99 ${percent}%, rgba(0,0,0,0) ${percent}%, rgba(0,0,0,0) 100%)`
+}
+
+var encounter = {
+    element: document.createElement("div"),
+    table: document.createElement("table"),
+    header: document.createElement("tr"),
+    metrics: encounterMetrics,
+
+    clear: function () {
+        while (this.table.rows[1]) this.table.deleteRow(1)
+    },
+    update: function (data) {
+        var tr = makeRow(this.metrics, data)
+        this.table.appendChild(tr)
+    },
+    makeHeader: function () {
+        makeHeader(this.metrics, this.header)
+    },
+    init: function () {
+        this.element.id = "encounter"
+        this.makeHeader()
+        this.table.appendChild(this.header)
+        this.element.appendChild(this.table)
+        content.appendChild(this.element)
+    },
+}
+var combatants = {
+    element: document.createElement("div"),
+    table: document.createElement("table"),
+    header: document.createElement("tr"),
+    metrics: combatantsMetrics,
+
+    clear: function () {
+        while (this.table.rows[1]) this.table.deleteRow(1)
+    },
+    update: function (data) {
+        var combatData = combatantDataSet(data)
+        combatData.forEach(function (row) {
+            var tr = makeRow(this.metrics, row)
+            tr.style.background = barBackground(row["name"], row["displayPercent"])
+            this.table.appendChild(tr)
+        }, this)
+    },
+    makeHeader: function () {
+        makeHeader(this.metrics, this.header)
+    },
+    init: function () {
+        this.element.id = "combatants"
+        this.makeHeader()
+        this.table.appendChild(this.header)
+        this.element.appendChild(this.table)
+        content.appendChild(this.element)
+    },
+}
